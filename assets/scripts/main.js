09f3942435c037ba559454e497f31bb24fcdeeb1
Evgeniy <KworkEvgeniy@yandex.ru>
2024-03-31 20:24:21 +0300

    начальный коммит

---
+++ assets/scripts/main.js
@@ -0,0 +1,16 @@
+/* Объявление глобальных переменных */
+const navbar__adaptive_menuBurger = document.getElementById("navbar__adaptive_menuBurger");
+const navbar__adaptive_menu = document.getElementById("navbar__adaptive_menu");
+const navbar__adaptive_menu_cross = document.getElementById("navbar__adaptive_menu_cross");
+
+
+/* Открытие и закрытие бургер меню */
+navbar__adaptive_menuBurger.addEventListener("click", () => {
+    navbar__adaptive_menu.classList.toggle("navbar__adaptive--active");
+    popup__background.classList.add("popup__background__active");
+})
+
+navbar__adaptive_menu_cross.addEventListener("click", () => {
+    navbar__adaptive_menu.classList.remove("navbar__adaptive--active");
+    popup__background.classList.remove("popup__background__active");
+})
